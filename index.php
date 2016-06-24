b7b4fd45edbc31ad2ad32e2659ca046646af4087
Will <will.buckingham@gmail.com>
2016-06-24 17:15:24 -0400

    Base commit of project with Vue includes.

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Blue Bite - Rotten Tomatoes Test</title>
+
+</head>
+<body>
+
+
+<script type="text/javascript" src="js/vue.js"></script><!--FOR DEV-->
+<!--<script src="//cdnjs.cloudflare.com/ajax/libs/vue/1.0.25/vue.min.js"></script>--><!--FOR LIVE-->
+<script src="//cdnjs.cloudflare.com/ajax/libs/vue-resource/0.9.0/vue-resource.min.js"></script>
+<script type="text/javascript">
+<!--
+	
+	
+	
+//-->
+</script>
+
+</body>
+</html>
